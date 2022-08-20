7f980cc6c1f5257e7dce85dabaa197bf41c63c19
Igor <datsenko2802@gmail.com>
2022-08-20 14:06:51 +0300

    update functional React Router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
+import ComicsPage from "../pages/ComicsPage";
+import MainPage from "../pages/MainPage";
 
 
 
 const App = () => {
 
-    
-
-        return (
+    return (
+        <BrowserRouter>
             <div className="app">
                 <AppHeader/>
                 <main>
-                   
-                    
+                   <Routes>
+                       <Route path="/" element={<MainPage/>}/>
+                       <Route path="/comics" element={<ComicsPage/>}/>
+                   </Routes>
                 </main>
             </div>
-        )
+         </BrowserRouter>
+    );
     
 }
 
